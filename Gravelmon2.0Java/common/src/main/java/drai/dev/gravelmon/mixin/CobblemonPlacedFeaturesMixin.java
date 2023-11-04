55edfcd8a87f71d05f401106b6a599bd18767b2f
StijnArts <JKT.Arts@student.han.nl>
2023-11-04 13:04:58 +0100

    2.0.4

--- Gravelmon2.0Java/common/src/main/java/drai/dev/gravelmon/mixin/CobblemonPlacedFeaturesMixin.java
+++ Gravelmon2.0Java/common/src/main/java/drai/dev/gravelmon/mixin/CobblemonPlacedFeaturesMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 @Mixin(CobblemonPlacedFeatures.class)
 public class CobblemonPlacedFeaturesMixin {
 
-    @Inject(method = "of", at =@At("HEAD"), remap = false, cancellable = true)
+    /*@Inject(method = "of", at =@At("HEAD"), remap = false, cancellable = true)
     private void ofReplacement(String id, CallbackInfoReturnable<ResourceKey<PlacedFeature>> cir){
         System.out.println("this mixin is active");
         if(id.equals("apricorn_trees")){
@@ -19,5 +19,5 @@ public class CobblemonPlacedFeaturesMixin {
             cir.setReturnValue(ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation("gravelmon",id)));
             cir.cancel();
         }
-    }
+    }*/
 }
