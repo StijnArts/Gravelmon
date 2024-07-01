95b3972ac0b366bc04506cca9d47342f2e4bbfe0
ASHISK404 <116161048+ASHISK404@users.noreply.github.com>
2024-07-01 13:01:46 +0800

    integrated mewthree and egho totodile line from Pokemon infinity
    - integrated mewthree from Pokemon infinity
    - integrated egho totodile line from Pokemon infinity

--- Gravelmon2.0Java/common/src/main/java/drai/dev/gravelmon/pokemon/infinity/regional/EghoTotodile.java
+++ Gravelmon2.0Java/common/src/main/java/drai/dev/gravelmon/pokemon/infinity/regional/EghoTotodile.java
@@ -134,5 +134,8 @@ public class EghoTotodile extends Pokemon {
         this.setHitbox(0.85, 1.4);
         this.setLangFileName("Totodile");
         this.setPortraitXYZ(0,1.8,0);
+        setModeled(true);
+        setBaseScale(1);
+        setHitbox(1,1);
     }
 }
