c5cd3c5cc5edebeac0b24ffd229a0c31acd5a1e8
StijnArts <JKT.Arts@student.han.nl>
2024-07-22 19:32:29 +0200

    mastenia stats

--- Gravelmon2.0Java/data/src/main/java/drai/dev/data/pokemon/mongratis/Robustaur.java
+++ Gravelmon2.0Java/data/src/main/java/drai/dev/data/pokemon/mongratis/Robustaur.java
@@ -6,15 +6,10 @@ import drai.dev.gravelmon.pokemon.attributes.*;
 
 import java.util.*;
 public class Robustaur extends drai.dev.data.pokemon.Pokemon {
-    public Robustaur() {
+    public Robustaur(Stats stats) {
         super("Robustaur",
                 Type.FIGHTING,
-                new Stats(0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0),
+                stats,
                 List.of(Ability.KEEN_EYE), Ability.KEEN_EYE,
                 0, 0,
                 new Stats(0,0,0,0,0,0), 0,
