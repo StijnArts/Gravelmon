9261b5a449d69e174488a2035c4e9ada4b1c292d
StijnArts <94121799+StijnArts@users.noreply.github.com>
2024-11-23 13:03:28 +0100

    fixed zip64 dependency and spawn writing

--- Gravelmon2.0Java/data/src/main/java/drai/dev/data/jsonwriters/data/SpawnPoolWorldJSONWriter.java
+++ Gravelmon2.0Java/data/src/main/java/drai/dev/data/jsonwriters/data/SpawnPoolWorldJSONWriter.java
@@ -91,8 +91,8 @@ public class SpawnPoolWorldJSONWriter {
                     condition.add("neededNearbyBlocks", neededNearbyBlocks);
                     pokemonSpawnData.preferredBlocks().forEach(neededNearbyBlocks::add);
                 }
-                spawns.add(spawnJson);
             }
+            spawns.add(spawnJson);
         }
     }
 
