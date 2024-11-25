70bf8db4ace9cfd16000af2bf9442401e395bc3c
StijnArts <94121799+StijnArts@users.noreply.github.com>
2024-11-25 07:09:38 +0100

    assetrs

--- Gravelmon2.0Java/common/src/main/java/drai/dev/gravelmon/apricorn/GravelmonApricornSaplingBlock.java
+++
@@ -1,9 +0,0 @@
-package drai.dev.gravelmon.apricorn;
-
-import net.minecraft.world.level.block.*;
-
-public class GravelmonApricornSaplingBlock extends SaplingBlock {
-    public GravelmonApricornSaplingBlock(Properties properties, GravelmonApricorns apricorn) {
-        super(new GravelmonApricornTreeGrower(apricorn), properties);
-    }
-}
